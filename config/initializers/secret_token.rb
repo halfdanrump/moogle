81ea7f82bdb089f14307566150582ca01b4ff2b2
Halfdan Rump <halfdan-rump@garage.co.jp>
2013-11-21 15:53:49 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moogle::Application.config.secret_key_base = '82704ea756b55b356f182abf101794c53eef0de833e16c042887755ff890dd2ec2f43b26423dfc6a98259487db8dae9288d2d284f9b76459094ce4e09a01df2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moogle::Application.config.session_store :cookie_store, key: '_moogle_session'
