4842d1cdea9108d3f931442d52e55330514512d9
Halfdan Rump <halfdan-rump@garage.co.jp>
2013-11-27 14:12:04 +0900

    Changes the c model a bit

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,6 +1,14 @@
 class Clinic
   include Mongoid::Document
-  field :code, type: Integer
-  field :name, type: String
-  field :postcode, type: Integer
+  field :fulldoc, type: String
+  field :h, as :hospital_code, type: String
+  field :n, as :name, type: String
+  field :p, as :postcode, type: Integer
+  field :departments, type: Array #File 2
+  field :facilities, type: Array #File 3
+  field :machines, type: Array #File 10
+
+
+
+  
 end
