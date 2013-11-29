bc27862d39fc74baa3fbefba24ef0099f30c1790
Halfdan Rump <halfdan-rump@garage.co.jp>
2013-11-29 15:42:16 +0900

    Executes migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129040102) do
+ActiveRecord::Schema.define(version: 20131129062831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clinics", force: true do |t|
+    t.string   "code"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "fulldocs", force: true do |t|
     t.string   "code"
